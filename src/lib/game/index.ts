acefc51ffafeabd2ec173d6b52b804deadc4a982
Amelia Harris <42555115+missmoode@users.noreply.github.com>
2022-05-11 20:16:16 +0100

    not sure

--- src/lib/game/index.ts
+++ src/lib/game/index.ts
@@ -1,1 +1,11 @@
-export * from './GameCanvas';
+/** @module Game */
+import { FixedViewport } from '../view';
+import { GameCanvas } from './GameCanvas';
+
+const canvas: GameCanvas = new GameCanvas(document.getElementsByTagName('main')[0]);
+
+export const Game = {
+  get rootStage(): FixedViewport {
+    return canvas.viewport;
+  }
+};
--- src/lib/network/NetworkSession.ts
+++ src/lib/network/NetworkSession.ts
@@ -1,3 +1,16 @@
-/**
- * Plan here is 
- */
+import Peer from 'peerjs';
+import { v4 as uuid } from 'uuid';
+/*
+  PeerJS only does peer-to-peer connections. Will have to leverage that to create a mesh network.
+*/
+class NetworkSession {
+  private name: ParticipantID;
+
+  constructor() {
+    this.name = uuid();
+    new Peer(this.name);
+  }
+
+  public joinRoomByParticipant(name: ParticipantID) {
+  }
+}
